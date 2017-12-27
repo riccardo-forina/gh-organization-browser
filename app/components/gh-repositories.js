0dfbc02aad9c269fc3764775001fc531f81d37fd
Riccardo Forina <r.forina@gmail.com>
2017-12-27 18:33:37 +0100

    Can filter repositories by language and visibility

--- app/components/gh-repositories.js
+++ app/components/gh-repositories.js
@@ -1,4 +1,80 @@
 import Component from '@ember/component';
 
+const NO_LANG = 'unknown';
+
 export default Component.extend({
+  init() {
+    this._super(...arguments);
+    this.languageChoices = [{
+      name: 'All languages',
+      value: '',
+      selected: !this.get('language')
+    }]
+  },
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.computeLanguages();
+    this.filterRepositories();
+  },
+
+  computeLanguages() {
+    const language = this.get('language');
+    this.get('repositories').then(repositories => {
+      const languages = repositories
+        .map((repo) => {
+          return repo.get('language') || NO_LANG;
+        });
+      const languageChoices = [{
+          name: 'All languages',
+          value: '',
+          selected: !language
+        },
+        ...languages.map(l => ({
+          name: l,
+          value: l,
+          selected: l === language
+        }))
+      ];
+      this.set("languageChoices", languageChoices);
+    });
+  },
+
+  filterRepositories() {
+    const language = this.get('language');
+    const languageFilter = language === NO_LANG ? null : language;
+    const showPublic = this.get('showPublic');
+    const showPrivate = this.get('showPrivate');
+    this.get('repositories').then(repositories => {
+      let filteredRepositories = repositories;
+      if (language) {
+        filteredRepositories = filteredRepositories.filter(r =>
+          r.get('language') === languageFilter
+        );
+      }
+      if (!showPrivate) {
+        filteredRepositories = filteredRepositories.filter(r =>
+          r.get('private') === false
+        )
+      }
+      if (!showPublic) {
+        filteredRepositories = filteredRepositories.filter(r =>
+          r.get('private') === true
+        )
+      }
+      this.set('filteredRepositories', filteredRepositories);
+    });
+  },
+
+  actions: {
+    onChangeLanguage(language) {
+      this.get('onChangeLanguage')(language);
+    },
+    onTogglePublic() {
+      this.get('onChangePublic')();
+    },
+    onTogglePrivate() {
+      this.get('onChangePrivate')();
+    }
+  }
 });
--- app/controllers/organization.js
+++ app/controllers/organization.js
@@ -13,12 +13,30 @@ export default Controller.extend({
     let language = this.get('language');
     let organization = this.get('model');
 
-    // if (category) {
-    //   return articles.filterBy('category', category);
-    // } else {
-    //   return articles;
-    // }
-    return organization.get('githubRepositories');
+    let allRepos = organization.get('githubRepositories');
+    let repos = [...allRepos];
+    if (language) {
+      repos = repos.filter(r => r.language === language);
+    }
+    if (showPublic) {
+      repos = repos.filter(r => r.private === false);
+    }
+    if (showPrivate) {
+      repos = repos.filter(r => r.private === true);
+    }
+    return repos;
   }),
+
+  actions: {
+    setLanguage(language) {
+      this.set('language', language);
+    },
+    togglePublic() {
+      this.toggleProperty('showPublic');
+    },
+    togglePrivate() {
+      this.toggleProperty('showPrivate');
+    }
+  }
 
 });
