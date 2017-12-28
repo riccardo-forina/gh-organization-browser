df0508a74e9ae0ed09e215ec8a56e3e088a9ec0f
Riccardo Forina <r.forina@gmail.com>
2017-12-28 12:40:39 +0100

    gh-repository tests

--- tests/integration/components/gh-repository-test.js
+++ tests/integration/components/gh-repository-test.js
@@ -1,24 +1,66 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from "ember";
+
+const testRepository = Ember.Object.create({
+  name: 'Baz',
+  description: 'Baz desc',
+  url: 'http://baz',
+  branches: ['master', 'dev', 'gh-pages'],
+  language: 'Typescript',
+  private: false
+});
 
 moduleForComponent('gh-repository', 'Integration | Component | gh repository', {
-  integration: true
+  integration: true,
+
+  beforeEach() {
+    this.set('name', testRepository.name);
+    this.set('description', testRepository.description);
+    this.set('url', testRepository.url);
+    this.set('branches', testRepository.branches);
+    this.set('language', testRepository.language);
+    this.set('private', testRepository.private);
+  }
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.render(hbs`{{gh-repository
+    name=name
+    description=description
+    url=url
+    branches=branches
+    language=language
+    private=private
+  }}`);
 
-  this.render(hbs`{{gh-repository}}`);
+  assert.equal(this.$().text().trim(), testRepository.name);
+});
 
-  assert.equal(this.$().text().trim(), '');
+test('it expands', function(assert) {
+  this.render(hbs`{{gh-repository
+    name=name
+    description=description
+    url=url
+    branches=branches
+    language=language
+    private=private
+  }}`);
 
-  // Template block usage:
-  this.render(hbs`
-    {{#gh-repository}}
-      template block text
-    {{/gh-repository}}
-  `);
+  this.$('.card-header').click();
+  assert.equal(this.$('.title').text().trim(), testRepository.name);
+});
 
-  assert.equal(this.$().text().trim(), 'template block text');
+test('it shows branch length', function(assert) {
+  this.render(hbs`{{gh-repository
+    name=name
+    description=description
+    url=url
+    branches=branches
+    language=language
+    private=private
+  }}`);
+
+  this.$('.card-header').click();
+  assert.equal(this.$('[data-branch-length]').text().trim(), 3);
 });
